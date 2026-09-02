a9bbdc5cd22843979f6854946c33255f694a1eb1
Gabriel Bonissoni <gabriel.bonissoni@live.com>
2026-09-02 10:22:16 -0300

    telas novas de emergencia e utilidade

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.zapbairro.app"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 6
-        versionName = "4.1"
+        versionCode = 7
+        versionName = "6"
     }
 
     // Novo DSL para Kotlin
